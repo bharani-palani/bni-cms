3179f59c8fdac6eb590bb0648857fb0e9142597e
Bharani Palani <barani.potshot@gmail.com>
2022-05-13 01:54:43 +0530

    database config phase 2 init

--- src/components/cms/sideMenu/Database/TableConfig.js
+++ src/components/cms/sideMenu/Database/TableConfig.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   Row,
   Col,
@@ -12,17 +12,10 @@ import {
   Dropdown,
 } from 'react-bootstrap';
 import { UserContext } from '../../../../contexts/UserContext';
+import _ from 'lodash';
 
 function TableConfig(props) {
   const userContext = useContext(UserContext);
-  const [formType, setFormType] = useState({
-    unsigned: false,
-    null: false,
-    auto_increment: false,
-    default: '',
-  });
-  const [selectedTypes, setSelectedTypes] = useState([]);
-  const [tableName, setTableName] = useState('');
   const inputTypeList = [
     { value: 'INT', label: 'INT' },
     { value: 'TINYINT', label: 'TINYINT' },
@@ -49,14 +42,52 @@ function TableConfig(props) {
     { value: 'SET', label: 'SET' },
     { value: 'BOOLEAN', label: 'BOOLEAN' },
   ];
-  const options = [
-    { label: 'Unsigned', key: 'unsigned', value: false },
-    { label: 'Null', key: 'null', value: false },
-    { label: 'Auto Increment', key: 'auto_increment', value: false },
-    { label: 'Default', key: 'default', value: '' },
-  ];
+  const defaultForm = {
+    field: '',
+    type: '',
+    constraint: '',
+    options: [
+      { label: 'Auto Increment', key: 'auto_increment', value: false },
+      { label: 'Unsigned', key: 'unsigned', value: false },
+      { label: 'Null', key: 'null', value: false },
+      { label: 'Default', key: 'default', value: '' },
+    ],
+    // note: focus here to set foreign keys in future
+    keys: [
+      { label: 'Primary key', key: 'primaryKey', value: false },
+      { label: 'Index', key: 'index', value: false },
+    ],
+  };
+  const [formType, setFormType] = useState(defaultForm);
+  const [selectedTypes, setSelectedTypes] = useState([]);
+  const [tableName, setTableName] = useState('');
+
   const addType = () => {
-    console.log('bbb', formType);
+    const newRow = {
+      field: formType.field,
+      type: formType.type,
+      constraint: formType.constraint,
+      options: formType.options
+        .filter(o => o.value)
+        .map(item => ({ [item.key]: item.value })),
+      keys: formType.keys.filter(f => f.value).map(item => item.key),
+    };
+    const bSelectedTypes = [...selectedTypes, newRow];
+    setSelectedTypes(bSelectedTypes);
+    setFormType(defaultForm);
+  };
+
+  const removeField = index => {
+    const filtered = [...selectedTypes].filter((_, i) => i !== index);
+    setSelectedTypes(filtered);
+  };
+
+  // useEffect(() => {
+  //   console.log('bbb', selectedTypes);
+  // }, [selectedTypes]);
+
+  const createTable = () => {
+    console.log('bbb', selectedTypes);
   };
 
   return (
@@ -82,93 +113,191 @@ function TableConfig(props) {
         </div>
       </Col>
       <Col md={10}>
-        <div className="py-2">Create Table</div>
-        <div className="col-lg-4 mb-2">
-          <InputGroup size="sm" className="mb-2">
-            <InputGroup.Text>Table name</InputGroup.Text>
-            <FormControl
-              value={tableName}
-              onChange={e => setTableName(e.target.value)}
-            />
-          </InputGroup>
-
-          <InputGroup size="sm">
-            <InputGroup.Text>Add field</InputGroup.Text>
-            <Form.Select
-              value={formType.type}
-              onChange={e =>
-                setFormType(prevState => ({
-                  ...prevState,
-                  type: e.target.value,
-                }))
-              }
+        <div className="py-2">
+          <div>Create Table</div>
+          <em>
+            <a
+              className="link-primary small"
+              href="https://dev.mysql.com/doc/refman/8.0/en/creating-tables.html"
+              rel="noreferrer"
+              target="_blank"
             >
-              <option>Select Type</option>
-              {inputTypeList.map((l, i) => (
-                <option value={l.value} key={i}>
-                  {l.label}
-                </option>
-              ))}
-            </Form.Select>
-            <FormControl
-              placeholder="Constraint"
-              type="number"
-              value={formType.constraint}
-              onChange={e =>
-                setFormType(prevState => ({
-                  ...prevState,
-                  constraint: e.target.value,
-                }))
-              }
-            />
-            <Dropdown autoClose="outside">
-              <Dropdown.Toggle id="dropdown-autoclose-true" />
-              <Dropdown.Menu variant="primary">
-                {options.map((o, i) => (
-                  <React.Fragment key={i}>
-                    <li className="p-1">
-                      {typeof o.value === 'boolean' ? (
-                        <div className="form-check">
-                          <input
-                            type="checkbox"
-                            className="form-check-input"
-                            id={o.label}
+              Read docs before creating one
+            </a>
+          </em>
+        </div>
+        <div className="mb-2">
+          <div className="col-md-3">
+            <InputGroup size="sm" className="mb-2">
+              <InputGroup.Text>Table name</InputGroup.Text>
+              <FormControl
+                value={tableName}
+                onChange={e => setTableName(e.target.value)}
+              />
+            </InputGroup>
+          </div>
+          <div className="col-lg-6">
+            <InputGroup size="sm" className="mb-2 col-md-6">
+              <FormControl
+                placeholder="Field name"
+                value={formType.field}
+                onChange={e =>
+                  setFormType(prevState => ({
+                    ...prevState,
+                    field: e.target.value,
+                  }))
+                }
+              />
+              <Form.Select
+                value={formType.type}
+                onChange={e =>
+                  setFormType(prevState => ({
+                    ...prevState,
+                    type: e.target.value,
+                  }))
+                }
+              >
+                <option value="">Select Type</option>
+                {inputTypeList.map((l, i) => (
+                  <option value={l.value} key={i}>
+                    {l.label}
+                  </option>
+                ))}
+              </Form.Select>
+              <FormControl
+                placeholder="Constraint"
+                type="number"
+                value={formType.constraint}
+                onChange={e =>
+                  setFormType(prevState => ({
+                    ...prevState,
+                    constraint: e.target.value,
+                  }))
+                }
+              />
+              <Dropdown autoClose="outside">
+                <Dropdown.Toggle id="dropdown-autoclose-true" />
+                <Dropdown.Menu variant="primary">
+                  <div className="p-1 small">
+                    {formType.options.map((o, i) => (
+                      <React.Fragment key={i}>
+                        {typeof o.value === 'boolean' ? (
+                          <div className="form-check">
+                            <input
+                              type="checkbox"
+                              className="form-check-input"
+                              id={o.label}
+                              name={o.key}
+                              checked={o.value}
+                              onChange={e => {
+                                setFormType(prevState => ({
+                                  ...prevState,
+                                  options: prevState.options.map(obj =>
+                                    obj.key === o.key
+                                      ? Object.assign(obj, {
+                                          value: e.target.checked,
+                                        })
+                                      : obj
+                                  ),
+                                }));
+                              }}
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor={o.label}
+                            >
+                              {o.label}
+                            </label>
+                          </div>
+                        ) : (
+                          <FormControl
+                            placeholder={o.label}
+                            size="sm"
                             value={o.value}
                             onChange={e =>
                               setFormType(prevState => ({
                                 ...prevState,
-                                [o.key]: e.target.checked,
+                                options: prevState.options.map(obj =>
+                                  obj.key === o.key
+                                    ? Object.assign(obj, {
+                                        value: e.target.value,
+                                      })
+                                    : obj
+                                ),
                               }))
                             }
                           />
-                          <label className="form-check-label" htmlFor={o.label}>
-                            {o.label}
-                          </label>
-                        </div>
-                      ) : (
-                        <FormControl
-                          placeholder={o.label}
-                          size="sm"
-                          defaultValue={o.value}
-                          onChange={e =>
-                            setFormType(prevState => ({
-                              ...prevState,
-                              [o.key]: e.target.value,
-                            }))
-                          }
-                        />
-                      )}
-                    </li>
-                  </React.Fragment>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-            <Button onClick={() => addType()}>
-              <i className="fa fa-plus" />
-            </Button>
-          </InputGroup>
+                        )}
+                      </React.Fragment>
+                    ))}
+                    <div className="p-1 fw-bold">Keys</div>
+                    {formType.keys.map((k, j) => (
+                      <Form.Check
+                        key={j}
+                        name="key"
+                        type={'radio'}
+                        label={k.label}
+                        id={k.key}
+                        checked={k.value}
+                        onChange={e =>
+                          setFormType(prevState => ({
+                            ...prevState,
+                            keys: prevState.keys.map(obj =>
+                              obj.key === k.key
+                                ? Object.assign(obj, {
+                                    value: e.target.checked,
+                                  })
+                                : obj
+                            ),
+                          }))
+                        }
+                      />
+                    ))}
+                  </div>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button
+                disabled={!(formType.field && formType.type)}
+                onClick={() => addType()}
+              >
+                <i className="fa fa-plus" />
+              </Button>
+            </InputGroup>
+          </div>
         </div>
-        <Button size="sm">Create</Button>
+        {selectedTypes.length > 0 && (
+          <div className="mb-2 col-lg-6">
+            <ListGroup>
+              {selectedTypes.map((a, i) => (
+                <ListGroup.Item
+                  key={i}
+                  className="d-flex justify-content-between align-items-center"
+                >
+                  <div>
+                    {a.keys.length > 0 && (
+                      <i className="fa fa-key text-danger me-2" />
+                    )}
+                    <span>{a.field}</span>
+                    <span className="badge bg-primary ms-2">
+                      {a.type} {a.constraint && `(${a.constraint})`}
+                    </span>
+                  </div>
+                  <i
+                    onClick={() => removeField(i)}
+                    className="fa fa-times cursor-pointer text-danger"
+                  />
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </div>
+        )}
+        <Button
+          onClick={() => createTable()}
+          disabled={!selectedTypes.length > 0}
+          size="sm"
+        >
+          Create
+        </Button>
         <div className="table-responsive">
           <table
             className={`table ${
