fc6e35bf502ed54bb79f78be122ff3dc748530de
Bharani Palani <barani.potshot@gmail.com>
2022-06-27 17:55:18 +0530

    react_app renamed

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,7 @@
 const baseUrl = () => {
   console.log('bbb', process.env);
-  const dev = process.env.React_App_LOCALHOST_BASE_URL;
-  const prod = process.env.React_App_PRODUCTION_BASE_URL;
+  const dev = process.env.REACT_APP_LOCALHOST_BASE_URL;
+  const prod = process.env.REACT_APP_PRODUCTION_BASE_URL;
   return process.env.NODE_ENV === 'development' ? dev : prod;
 };
 
