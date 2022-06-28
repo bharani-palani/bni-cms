984c50c4b4b1aa05c80a07fa2d57ad70c8c96517
Bharani Palani <barani.potshot@gmail.com>
2022-06-28 15:17:08 +0530

    conf 1

--- copyServices.js
+++ copyServices.js
@@ -1,8 +1,12 @@
 const copyfiles = require('copyfiles');
 
-copyfiles(['./services/**/*', './build'], '-a -F -u 1', () => {
-  console.log('Awzy API service directory is ready for production..');
-});
+copyfiles(
+  ['./services/**/*', './services/**/.*', './build'],
+  '-a -F -u 1',
+  () => {
+    console.log('Awzy API service directory is ready for production..');
+  }
+);
 
 copyfiles(['./.htaccess', './build'], () => {
   console.log(
