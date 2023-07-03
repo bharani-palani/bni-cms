338761dd70696a1d76bd0d2c162ead3be3de6275
bharani-palani <tp.bharani@gmail.com>
2023-07-03 18:12:15 +0530

    build 2

---
+++ build/services/application/models/home_model.php
@@ -0,0 +1,247 @@
+<?php
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+class home_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        @$this->db = $this->load->database('default', true);
+    }
+    public function get_config()
+    {
+        $query = $this->db->get('az_config');
+        return get_all_rows($query);
+    }
+    public function fetchAccessLevels()
+    {
+        $this->db->where('access_value !=', 'public');
+        $query = $this->db->get('az_access_levels');
+        return get_all_rows($query);
+    }
+    public function fetchUsers()
+    {
+        $this->db
+            ->select(
+                [
+                    'a.user_id as user_id',
+                    'a.user_name as user_name',
+                    'a.user_display_name as user_display_name',
+                    'a.user_profile_name as user_profile_name',
+                    'a.user_email as user_email',
+                    'a.user_mobile as user_mobile',
+                    'a.user_image_url as user_image_url',
+                    'b.access_label as user_type',
+                    'a.user_is_founder as user_is_founder',
+                ],
+                false
+            )
+            ->from('az_users as a')
+            ->join('az_access_levels as b', 'a.user_type = b.access_id')
+            ->group_by(['a.user_id']);
+        $query = $this->db->get();
+        return get_all_rows($query);
+    }
+    public function validateUser($post)
+    {
+        $this->db
+            ->select(
+                [
+                    'a.user_id as user_id',
+                    'a.user_display_name as user_display_name',
+                    'a.user_profile_name as user_profile_name',
+                    'a.user_email as user_email',
+                    'a.user_mobile as user_mobile',
+                    'b.access_value as user_type',
+                    'a.user_image_url as user_image_url',
+                    'a.user_last_login as user_last_login',
+                    'a.user_current_login as user_current_login',
+                ],
+                false
+            )
+            ->from('az_users as a')
+            ->join('az_access_levels as b', 'a.user_type = b.access_id')
+            ->where('a.user_name', $post['username'])
+            ->where('a.user_password', md5($post['password']))
+            ->group_by(['a.user_id']);
+        $query = $this->db->get();
+
+        if ($query->num_rows > 0) {
+            $row = $query->row();
+            $user_current_login = $row->user_current_login;
+            $user_id = $row->user_id;
+
+            $data = [
+                'user_last_login' => $user_current_login,
+                'user_current_login' => date('Y-m-d H:i:s'),
+            ];
+
+            $this->db->where('user_id', $user_id);
+            $this->db->update('az_users', $data);
+
+            return [
+                'user_id' => $row->user_id,
+                'user_display_name' => $row->user_display_name,
+                'user_profile_name' => $row->user_profile_name,
+                'user_email' => $row->user_email,
+                'user_mobile' => $row->user_mobile,
+                'user_type' => $row->user_type,
+                'user_image_url' => $row->user_image_url,
+                'user_last_login' => $row->user_last_login,
+                'user_current_login' => $row->user_current_login,
+            ];
+        } else {
+            return false;
+        }
+    }
+    public function checkUserExists($post)
+    {
+        $this->db->where('user_name =', $post['username']);
+        $this->db->or_where('user_email =', $post['email']);
+        $query = $this->db->get('az_users');
+        if ($query->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function changePassword($post)
+    {
+        $query = $this->db->get_where('az_users', [
+            'user_name' => $post['userName'],
+            'user_password' => md5($post['currentPass']),
+        ]);
+        if ($query->num_rows > 0) {
+            $this->db->where('user_name', $post['userName']);
+            $this->db->update('az_users', [
+                'user_password' => md5($post['newPass']),
+            ]);
+            if ($this->db->affected_rows() > 0) {
+                return ['status' => true];
+            } else {
+                return ['status' => false];
+            }
+        } else {
+            return ['status' => false];
+        }
+    }
+    public function checkValidEmail($post)
+    {
+        $query = $this->db->get_where('az_users', [
+            'user_email' => $post['email'],
+        ]);
+        if ($query->num_rows > 0) {
+            $result = $query->row();
+            return $result->user_id;
+        } else {
+            return false;
+        }
+    }
+    public function validateOtpTime($post)
+    {
+        $this->db->where([
+            'user_id' => $post['id'],
+            'user_otp' => $post['otp'],
+            'user_otp_expiry >' => time(),
+        ]);
+        $query = $this->db->get('az_users');
+        if ($query->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function resetUpdate($userId, $resetPassword)
+    {
+        $this->db->where('user_id', $userId);
+        $this->db->update('az_users', ['user_password' => md5($resetPassword)]);
+        if ($this->db->affected_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function otpUpdate($userId, $otp)
+    {
+        $this->db->where('user_id', $userId);
+        $this->db->update('az_users', [
+            'user_otp' => $otp,
+            'user_otp_expiry' => strtotime('+5 minutes', time()),
+        ]);
+        if ($this->db->affected_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function getBackend($post)
+    {
+        $Table = $post['Table'];
+        $this->db->select($post['TableRows']);
+        switch ($Table) {
+            case 'config':
+                $query = $this->db->get('az_config');
+                break;
+            case 'users':
+                $query = $this->db->get_where('az_users', []);
+                break;
+            default:
+                return false;
+        }
+        return get_all_rows($query);
+    }
+    public function postBackend($post)
+    {
+        $postData = json_decode($post['postData']);
+        $Table = $postData->Table;
+        switch ($Table) {
+            case 'config':
+                return $this->onTransaction(
+                    $postData,
+                    'az_config',
+                    'config_id'
+                );
+                break;
+            case 'users':
+                return $this->onTransaction($postData, 'az_users', 'user_id');
+                break;
+            default:
+                return false;
+        }
+    }
+    public function onTransaction($postData, $table, $primary_field)
+    {
+        $this->db->trans_start();
+        if (isset($postData->updateData) && count($postData->updateData) > 0) {
+            $array = json_decode(json_encode($postData->updateData), true);
+            $this->db->update_batch($table, $array, $primary_field);
+        }
+        if (isset($postData->insertData) && count($postData->insertData) > 0) {
+            $array = json_decode(json_encode($postData->insertData), true);
+            $this->db->insert_batch($table, $array);
+        }
+        if (isset($postData->deleteData) && count($postData->deleteData) > 0) {
+            $array = json_decode(json_encode($postData->deleteData), true);
+            $this->db->where_in($primary_field, $array);
+            $this->db->delete($table);
+        }
+        $this->db->trans_complete();
+        return $this->db->trans_status() === false ? false : true;
+    }
+
+    public function saveLog($post)
+    {
+        $this->db->insert('az_logs', [
+            'log_id' => NULL,
+            'log_name' => $post->name,
+            'log_email' => $post->email,
+            'log_source' => $post->source,
+            'log_type' => $post->type,
+            'log_user_id' => $post->userId,
+            'log_time' => $post->time,
+            'log_ip' => $post->ip,
+        ]);
+        return $this->db->affected_rows() > 0;
+    }
+}
